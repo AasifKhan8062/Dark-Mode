0cf84d3aff8d460efe6668604e9a4cfa4f2022cf
Aasif khan <149509037+AasifKhan8062@users.noreply.github.com>
2024-04-21 18:05:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector(".body");
+let light = document.querySelector(".light").addEventListener("click",()=>{
+  document.body.style.backgroundColor = "white";
+})
+let Dark = document.querySelector(".Dark").addEventListener("click",()=>{
+  document.body.style.backgroundColor = "black";
+})
